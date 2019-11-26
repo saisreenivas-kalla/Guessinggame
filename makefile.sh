e96c7b9520dbd1f254b2e00d2bc2cc50a455b37f
saisreenivas-kalla <saisreenivas7@gmail.com>
2019-11-26 16:46:26 +0530

    added readme.md

--- makefile.sh
+++ makefile.sh
@@ -1,6 +1,7 @@
 git init
 echo -e "# The name of the project is Guessing game \n" > readme.md
 date >> readme.md
-echo -e "\n ## The program contains 24 lines of code" >> readme.md
+a=wc -l guessinggame.sh | egrep -o "[0-9]+"
+echo -e "\n ## The program contains $a lines of code" >> readme.md
 git add -A
 git commit -m "added readme.md"
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # The name of the project is Guessing game 
 
-Tue Nov 26 16:44:43 IST 2019
+Tue Nov 26 16:46:25 IST 2019
 
- ## The program contains 24 lines of code
+ ## The program contains  lines of code
